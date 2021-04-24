9bff2e63ed99d5e14e427a1cb39b723ec43b64b4
Ansuman-mishra <66157597+Ansuman-mishra@users.noreply.github.com>
2021-04-24 23:52:23 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -29,7 +29,7 @@ class Navbar extends Component {
                >
                   <ul className="navbar-nav">
                      <li className="nav-item">
-                        <Link className="nav-link" to="/home">
+                        <Link className="nav-link" to="/">
                            Home <span className="sr-only">(current)</span>
                         </Link>
                      </li>
